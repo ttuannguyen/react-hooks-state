fe801de15117cf7ace2bdb5de5e13289a705a054
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-03-01 17:19:53 -0500

    commit tree 243185d9b2929edfa31437e53ce3e53f2ccc4b15

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, {useState} from "react"; //importing useState hook also
 
 function Counter() {
   const [count, setCount] = useState(0);
-
+  //count = ref to current value of that state 
+  //setCount = setter function so we can update that state
+  
+  
   function increment() {
-    setCount(count + 1);
+    
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+  
   }
-
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  
+  return <button onClick={increment}>{count}</button>; //using count var to dispay current state in <button> element 
 }
+
+
 
 export default Counter;
